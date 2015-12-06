dc5c758b95479550f0da4cf9070a65f8aff568ed
shinsaku_takesako <takesako@gaprise.com>
2015-12-06 17:47:11 +0000

    implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+    #名前　必須入力かつ20文字以内
+    validates :name , length: { maximum: 20 } , presence: true
+    #内容　必須入力かつ2文字以上30文字以下
+    validates :body , length: { minimum: 2, maximum: 20 } , presence: true
 end
